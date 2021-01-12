40639ebf27f32eb961cd2790a91ff5ddaa8ef3b8
JHK <31474272+kjhg478@users.noreply.github.com>
2021-01-12 16:18:34 +0900

    https://github.com/kjhg478/weather_2021.git

---
+++ Loading.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Loding() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>잠자코 기다려라</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "flex-end",
+        paddingHorizontal: 30,
+        paddingVertical: 100,
+        backgroundColor: '#FDF6AA'
+    },
+
+    text: {
+        color: "#2c2c2c",
+        fontSize: 25
+    }
+});
